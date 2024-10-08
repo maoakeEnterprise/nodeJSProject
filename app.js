6b3fd79fff1139062b4ea7120b71bf6a476938b5
Maoake TERIIEROOITERAI <90271704+maoakeEnterprise@users.noreply.github.com>
2024-10-08 13:40:24 +0200

    Avancement de l'apprentissage NodeJS

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 const express = require('express')
+const morgan = require('morgan')
+const favicon = require('serve-favicon')
 let pokemons = require('./src/db/mock-pokemon')
-
+const { success } = require('./helper.js')
 const app = express()
 const port = 3000
 
+app
+    .use(favicon(__dirname + '/src/favicon.ico'))
+    .use(morgan('dev'))
 
-/*
-Formule app.{METHODE}({ROUTE}, {CONFIGURATION}) 
-GET sert a appeler une route qui sera configuré dans l'URL puis il y aura une methode de configuré dans cette méthode
-LISTEN sert a ecouter le port qu'on a emprunter pour lancer le site en localhost
-*/
+app.get('/', (req, res) => res.send('Hello again express'))
 
-app.get('/', (req,res) => res.send('Hello again express'))
+app.get('/api/pokemons', (req, res) => {
+    const message = `Il y a ${pokemons.length} pokemon dans le pokedex pour le moment.`
+    res.json(success(message, pokemons))
+})
 
-app.get('/api/pokemons', (req,res) => {
-    const pokemonsLenght = pokemons.length
-    
-    res.send(`Il y a ${pokemonsLenght} pokemon dans le pokedex pour le moment.`)
-}) 
+app.get('/api/pokemons/:id', (req, res) => {
+    const message = "Un pokemon a bien été trouvé"
+    const pokemon = pokemons.find(pokemon => pokemon.id === parseInt(req.params.id))
 
-app.get('/api/pokemons/:id', (req,res) => {
-    const id = parseInt(req.params.id)
-
-    const pokemon = pokemons.find(pokemon => pokemon.id === id)
-
-    res.send(`Vous avez demandé le pokémon ${pokemon.name}.`)
+    res.json(success(message, pokemon))
 })
 
 app.listen(port, () => console.log(`Notre application Node est sur : http://localhost:${port}`))
